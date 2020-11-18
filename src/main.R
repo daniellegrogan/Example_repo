c56624dc06c40fd531cee2c6673ef220b063626c
Danielle Grogan <dgrogan@squam.sr.unh.edu>
2020-11-18 10:27:44 -0500

    initial commit

---
+++ src/main.R
@@ -0,0 +1,14 @@
+# main.R
+
+# description of what's in this file
+# Author: Danielle S Grogan, University of New Hampshire
+
+# libraries
+
+
+
+# source some function
+source("src/some_function.R")
+
+# code here:
+# do some stuff
